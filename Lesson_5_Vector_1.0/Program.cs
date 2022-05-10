b4d225ee2e09b49253e10eca18652aeca09c070c
atrushin1983@gmail.com <atrushin1983@gmail.com>
2022-05-10 13:55:20 +0700

    добавил переносы строк

--- Lesson_5_Vector_1.0/Program.cs
+++ Lesson_5_Vector_1.0/Program.cs
@@ -1,23 +1,23 @@
 ﻿
     Console.WriteLine("Введите координаты вектора А:");// честно говоря, я не совсем понял, зачем в этом ДЗ создавать классы и конструкторы
-    int x1 = Getinput1("Введите значение |x1|: ");     //поэтому сделал по своему.
+    int x1 = Getinput1("\nВведите значение |x1|: ");     //поэтому сделал по своему.
     int y1 = Getinput1("Введите значение |y1|: ");
     int z1 = Getinput1("Введите значение |z1|: ");
 
 
-    Console.WriteLine("Введите координаты вектора B:");
-    int x2 = Getinput1("Введите значение |x2|:");
+    Console.WriteLine("\nВведите координаты вектора B:");
+    int x2 = Getinput1("\nВведите значение |x2|:");
     int y2 = Getinput1("Введите значение |y2|:");
     int z2 = Getinput1("Введите значение |z2|:");
 
    
-    Console.WriteLine($"координаты вектора A: |x1= {x1}||y1= {y1}||z1= {z1}|");
+    Console.WriteLine($"\nкоординаты вектора A: |x1= {x1}||y1= {y1}||z1= {z1}|");
     Console.WriteLine($"координаты второго B: |x2= {x2}||y2= {y2}||z2= {z2}|");
   
     
-    Console.WriteLine("Вектора A и B");
+    Console.WriteLine("\nВектора A и B");
     var value = Math.Sqrt(x1 * x1 + y1 * y1 + z1 * z1);
-    Console.WriteLine($"длина вектора А = {value} ");
+    Console.WriteLine($"\nдлина вектора А = {value} ");
     var value1 = Math.Sqrt(x2 * x2 + y2 * y2 + z2 * z2);
     Console.WriteLine($"длина вектора B = {value1}");
     var value3 = x1 * x2 + y1 * y2 + z1 * z2;
